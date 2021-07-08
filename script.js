e660554d7e9afd8f84c8416dac79a9e3e9ee158d
Rishav Srivastava <66356982+rishavsr99@users.noreply.github.com>
2021-07-08 20:59:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function showmenu(){
+    document.getElementById('side-menu').style.display="block";
+    document.getElementById('bar1').style.display="none";
+    document.getElementById('bar2').style.display="block";
+}
+
+function hidemenu(){
+    document.getElementById('side-menu').style.display="none";
+    document.getElementById('bar1').style.display="block";
+    document.getElementById('bar2').style.display="none";
+}
+
+function maywood_2(){
+    document.getElementById('photo1').style.display="none";
+    document.getElementById('photo2').style.display="block";
+    document.getElementById('photo3').style.display="none";
+}
+
+function maywood_1(){
+    document.getElementById('photo1').style.display="block";
+    document.getElementById('photo2').style.display="none";
+    document.getElementById('photo3').style.display="none";
+}
+
+function maywood_3(){
+    document.getElementById('photo1').style.display="none";
+    document.getElementById('photo2').style.display="none";
+    document.getElementById('photo3').style.display="block";
+}
+
+
+function beverly_2(){
+    document.getElementById('photox1').style.display="none";
+    document.getElementById('photox2').style.display="block";
+    document.getElementById('photox3').style.display="none";
+}
+
+function beverly_1(){
+    document.getElementById('photox1').style.display="block";
+    document.getElementById('photox2').style.display="none";
+    document.getElementById('photox3').style.display="none";
+}
+
+function beverly_3(){
+    document.getElementById('photox1').style.display="none";
+    document.getElementById('photox2').style.display="none";
+    document.getElementById('photox3').style.display="block";
+}
